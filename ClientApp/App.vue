4ec4d317c5f2b9d6878d517be28a68efd2ec4f5e
roy <jnmcluo@163.com>
2018-12-31 10:19:17 +0800

    init

---
+++ ClientApp/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="home">
+        <h1>This is a Simple DotnetCore-Vue Biloplate, Enjoy!</h1>
+        <h6>by Roy Morrison , 1st day,2019</h6>
+    </div>
+</template>
+
+<script>
+export default { }
+</script>
+
+<style lang="scss">
+
+</style>
+ 
